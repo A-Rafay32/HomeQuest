20c032eb3d5e43b9c8c427caecfc97fa3261a126
A-Rafay32 <abdulrafaychohan986@gmail.com>
2024-06-10 00:22:14 +0500

    update

--- lib/features/home/screens/widgets/popular_houses_card.dart
+++ lib/features/home/screens/widgets/popular_houses_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:real_estate_app/app/themes/app_styles.dart';
 import 'package:real_estate_app/app/themes/app_colors.dart';
 import 'package:real_estate_app/app/themes/app_paddings.dart';
